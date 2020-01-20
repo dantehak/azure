96c54f3bcf68dd590b1febfce6ce9626431abbe9
dantehak <52209917+dantehak@users.noreply.github.com>
2020-01-20 15:02:01 +0100

    a lot of changes

---
+++ assesment/Permissions.ps1
@@ -0,0 +1,4 @@
+$webappname = Read-Host -Prompt 'Geef aangemaakte web app naam in'
+$resourcegroup = Read-Host -Prompt 'Geef aangemaakt Resource Groep naam in.'
+$id = (Get-AzADServicePrincipal -DisplayName "$webappname").id
+New-AzRoleAssignment -ObjectId "$id" -RoleDefinitionName contributor -ResourceGroupName "$resourcegroup"
