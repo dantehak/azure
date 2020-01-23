e78408425b1f54b979a83d3183baf375bc4d5299
dantehak <52209917+dantehak@users.noreply.github.com>
2020-01-23 14:16:25 +0100

    Removed file.

--- assesment/Permissions.ps1
+++ assesment/Permissions.ps1
@@ -1,3 +1,4 @@
+Connect-AzAccount
 $webappname = Read-Host -Prompt 'Geef aangemaakte web app naam in'
 $resourcegroup = Read-Host -Prompt 'Geef aangemaakt Resource Groep naam in.'
 $id = (Get-AzADServicePrincipal -DisplayName "$webappname").id
